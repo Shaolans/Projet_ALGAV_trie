40b3faacdcf366942b9382133e22ab3dcc8a148e
aarkoub <Amel^^@Amel-pc>
2017-11-19 01:07:54 +0100

    Add word OK

--- Projet_ALGAV_trie/src/patriciaTrie/structure/PatriciaTrie.java
+++ Projet_ALGAV_trie/src/patriciaTrie/structure/PatriciaTrie.java
@@ -1,12 +1,107 @@
 package patriciaTrie.structure;
 
-import java.util.List;
 
-import patriciaTrie.interfaces.IPatriciaTrie;;
-
-public class PatriciaTrie implements IPatriciaTrie{
+public class PatriciaTrie {
 	
-	private Node[] nodes = new Node[26];
+	private PatriciaTrie[] patTries = new PatriciaTrie[27];
+	private int ind;
+	private String feuille;
+	
+	public PatriciaTrie(){
+		ind=0;
+	}
+	
+	private PatriciaTrie(int ind){
+		this.ind=ind;
+	}
+
+	
+	public void addWord(String word) {
+		char c = word.charAt(ind);
+		if(patTries[c-97]==null){
+			patTries[c-97] = new PatriciaTrie(ind+1);
+			patTries[c-97].feuille = word;
+			return;
+		}
+		
+		if(patTries[c-97].feuille==null ){
+			for(int i=ind; i<word.length(); i++){
+				if(patTries[word.charAt(i)-97]!=null && i==ind){
+					patTries[word.charAt(i)-97].addWord(word);
+					break;
+				}
+			}
+			
+		}
+		else{
+			
+			String mot1, mot2;
+			if(patTries[c-97].feuille.length()<word.length()){
+				mot1=patTries[c-97].feuille;
+				mot2=word;
+			}
+			else{
+				mot1=word;
+				mot2=patTries[c-97].feuille;
+			}
+			int i;
+			for(i=0; i<mot1.length(); i++){
+				if(mot1.charAt(i)!=mot2.charAt(i)){
+					break;
+				}
+			}
+			if(i==mot1.length()){
+				patTries[c-97].patTries[26]=new PatriciaTrie(i);
+				patTries[c-97].patTries[mot2.charAt(i)-97]=new PatriciaTrie(i);
+				patTries[c-97].patTries[26].feuille = mot1;
+				patTries[c-97].patTries[mot2.charAt(i)-97].feuille = mot2;
+			}
+			else{
+				patTries[c-97].patTries[mot1.charAt(i)-97]=new PatriciaTrie(i);
+				patTries[c-97].patTries[mot2.charAt(i)-97]=new PatriciaTrie(i);
+				patTries[c-97].patTries[mot1.charAt(i)-97].feuille = mot1;
+				patTries[c-97].patTries[mot2.charAt(i)-97].feuille = mot2;
+			}
+			patTries[c-97].feuille=null;
+			patTries[c-97].ind=i;
+		}
+		
+	}
+	
+	@Override
+	public String toString(){
+		
+		StringBuilder word = new StringBuilder();
+		for(int i=0; i<27; i++){
+			if(patTries[i]!=null){
+				word.append("niveau " +((char)(i+97))+" "+ind+"\n");
+				if(patTries[i].feuille!=null){
+					word.append(patTries[i].feuille);
+					word.append("\n");
+				}
+				else{
+					word.append(patTries[i].toString());
+				}
+			}
+		}
+		
+		return word.toString();
+		
+	}
+
+	
+	public boolean findWord(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean deleteWord(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	/*private Node[] nodes = new Node[26];
 	String endChar = "~";
 	
 	public PatriciaTrie(){	}
@@ -33,13 +128,13 @@ public class PatriciaTrie implements IPatriciaTrie{
 			valeurNoeud = currentNode.getValeur();
 
 			while(i<valeurNoeud.length() && j<word.length() ){
-				//si on atteint un caractère final dans le noeud
+				//si on atteint un caractï¿½re final dans le noeud
 				if(valeurNoeud.charAt(i)==endChar.charAt(0)){
 					findEnd=true;
 					break;
 				}
 				
-				//quand on atteint le premier caractère qui diffère dans le noeud
+				//quand on atteint le premier caractï¿½re qui diffï¿½re dans le noeud
 				if(valeurNoeud.charAt(i)!=word.charAt(j)){
 					break;
 				}
@@ -116,7 +211,7 @@ public class PatriciaTrie implements IPatriciaTrie{
 		
 		return word.toString();
 		
-	}
+	}*/
 
 	
 }
