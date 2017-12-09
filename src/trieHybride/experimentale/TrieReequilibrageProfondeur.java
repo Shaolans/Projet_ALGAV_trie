42e502e8a3aa06d93976b7c27788d7dc7c8b6dea
L-C <L-C@DESKTOP-UKL1B7D>
2017-12-09 21:39:43 +0100

    [ADD] calcul reequilibrage

---
+++ src/trieHybride/experimentale/TrieReequilibrageProfondeur.java
@@ -0,0 +1,34 @@
+package trieHybride.experimentale;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import trie.file.TrieLoadFile;
+import trie.file.TriePrintFile;
+import trieHybride.implementation.TrieHybride;
+import trieHybride.implementation.TrieHybridePrimitive;
+import trieHybride.interfaces.ITrieHybride;
+
+public class TrieReequilibrageProfondeur {
+
+	public static void main(String[] args) {
+		List<String> shakespeare = TrieLoadFile.loadWordsShakespeare();
+		List<String> depth = new ArrayList<>();
+		int pas = 1000;
+		int max = shakespeare.size();
+		ITrieHybride root = new TrieHybride();
+		
+		for(int i = 0; i < max; i++){
+			TrieHybridePrimitive.ajoutMot(shakespeare.get(i), root);
+			if(i%pas == 0){
+				TrieHybridePrimitive.balanceTrieHybride(root);
+				depth.add(i+" "+TrieHybridePrimitive.profondeurMoyenne(root));
+			}
+		}
+		
+		
+		TriePrintFile.printFile("comparaison/trie_hybride_reequilibrage_profondeur_moyenne.txt", depth);
+
+	}
+
+}
