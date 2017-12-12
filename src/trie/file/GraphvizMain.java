78c3d8bc2de4721729b6573fa5e8ac2c252f2e1a
L-C <L-C@DESKTOP-UKL1B7D>
2017-12-12 22:45:14 +0100

    [MOD] Refactoring et ajout d'exemple

--- src/trie/file/GraphvizMain.java
+++ src/trie/file/GraphvizMain.java
@@ -7,6 +7,13 @@ import trieHybride.interfaces.ITrieHybride;
 public class GraphvizMain {
 
 	public static void main(String[] args) {
+		GraphvizMain.graphExempleDeBase();
+		GraphvizMain.graphTest1();
+		
+	}
+	
+	
+	public static void graphExempleDeBase() {
 		ITrieHybride root = TrieHybridePrimitive.initTrieHybride();
 		root = TrieLoadFile.loadFileTrieHybride("files/exemple_de_base");
 		GraphvizPrint.printTrieHybrideGraph("graphviz/th_exemple_de_base.dot", root);
@@ -14,6 +21,31 @@ public class GraphvizMain {
 		PatriciaTrie pt = new PatriciaTrie();
 		pt = TrieLoadFile.loadFilePatriciaTrie("files/exemple_de_base");
 		GraphvizPrint.printPatriciaTrieGraph("graphviz/pt_exemple_de_base.dot", pt);
+	}
+	
+	public static void graphTest1() {
+		ITrieHybride root = TrieHybridePrimitive.initTrieHybride();
+		TrieHybridePrimitive.ajoutMot("ceci", root);
+		TrieHybridePrimitive.ajoutMot("est", root);
+		TrieHybridePrimitive.ajoutMot("la", root);
+		TrieHybridePrimitive.ajoutMot("representation", root);
+		TrieHybridePrimitive.ajoutMot("du", root);
+		TrieHybridePrimitive.ajoutMot("trie", root);
+		TrieHybridePrimitive.ajoutMot("structure", root);
+		TrieHybridePrimitive.ajoutMot("reprise", root);
+		GraphvizPrint.printTrieHybrideGraph("graphviz/th_exemple_1.dot", root);
+		
+		PatriciaTrie pt = new PatriciaTrie();
+		PatriciaTrie.ajouterMot(pt, "ceci");
+		PatriciaTrie.ajouterMot(pt, "est");
+		PatriciaTrie.ajouterMot(pt, "la");
+		PatriciaTrie.ajouterMot(pt, "representation");
+		PatriciaTrie.ajouterMot(pt, "du");
+		PatriciaTrie.ajouterMot(pt, "trie");
+		PatriciaTrie.ajouterMot(pt, "structure");
+		PatriciaTrie.ajouterMot(pt, "reprise");
+		GraphvizPrint.printPatriciaTrieGraph("graphviz/pt_exemple_1.dot", pt);
+		
 		
 	}
 
